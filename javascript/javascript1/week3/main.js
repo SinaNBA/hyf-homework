93b737cbbe59f1db1d65a2cb77d9adaf28c26818
SinaNBA <snejadbakhtiari@gmail.com>
2019-03-28 02:48:55 +0100

    Week3 homework modified

--- javascript/javascript1/week3/main.js
+++ javascript/javascript1/week3/main.js
@@ -137,6 +137,8 @@ function addNote(content, id) {
     };
     notes.push(contentId);
 }
+addNote('my note 1', 1);
+addNote('my note 2', 2);
 
 function getNoteFromId(id) {
     for (var i = 0; i < notes.length; i++) {
@@ -147,3 +149,73 @@ function getNoteFromId(id) {
         }
     }
 }
+
+console.log(getNoteFromId(1));
+
+function getAllNotes() {
+    return notes;
+}
+
+console.log(getAllNotes());
+
+function logOutNotesFormatted() {
+    for (var i = 0; i < notes.length; i++) {
+        return "The note with id: " + notes[i].id + " , has the following note text: " + notes[i].content + ".";
+    }
+}
+
+console.log(logOutNotesFormatted());
+
+//CactusIO-interactive (Smart phone usage app)
+
+var activities = [];
+
+function addActivity(date, activity, duration) {
+    if (date == null) {
+        let d = new Date();
+        let day = (d.getDate()).toString();
+        let month = (d.getMonth() + 1).toString();
+        let year = (d.getFullYear()).toString();
+        year = year.slice(2, 4);
+        date = (day + '/' + month + '-' + year).toString();
+    }
+    let activityObj = {
+        'date': date,
+        'activity': activity,
+        'duration': duration
+    }
+    activities.push(activityObj);
+}
+
+addActivity('23/7-18', 'Youtube', 30);
+addActivity('24/7-18', 'Youtube', 30);
+addActivity('25/7-18', 'Youtube', 30);
+console.log(activities);
+
+function showStatus(activity) {
+    var j = 0;
+    var mins = 0;
+    var setLimit = 90;
+    for (let i = 0; i < activities.length; i++) {
+        if (activities[i]['activity'] === activity) {
+            j++;
+            mins += activities[i]['duration'];
+        }
+    }
+    if (mins > setLimit) {
+        return "You have added " + j + " activities. They amount to " + mins + " min of usage \n You have reached your limit, no more smartphoning for you!";
+    } else if (j === 0) {
+        return "Add some activities before calling showStatus";
+    } else {
+        return "You have added " + j + " activities. They amount to " + mins + " min of usage";
+    }
+}
+
+console.log(showStatus('Youtube'));
+console.log(showStatus('instagram')); // For testing if there is an empty activities
+
+addActivity('26/7-18', 'Youtube', 30);
+console.log(showStatus('Youtube'));
+
+addActivity(null, 'Facebook', 20); // For testing if there is an empty date
+console.log(activities);
