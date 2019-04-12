db120667dc50ed1e5a19fbd0bfa821632eee62e0
Sina Nejadbakhtiari <snejadbakhtiari@gmail.com>
2019-04-12 17:49:44 +0200

    Week6 homework added

---
+++ javascript/javascript2/week6/Mentors/data.js
@@ -0,0 +1,8 @@
+const mentors = [
+    { "name": "Abed Sujan", "subject": ['JS', 'HTML', 'CSS', 'NODEJS']},
+    { "name": "Ahmed Magdy", "subject": ['JS', 'Database', 'CSS']},
+    { "name": "Alicia Gonzales", "subject": ['DB', 'HTML', 'NODEJA']},
+    { "name": "Allan Thraen", "subject": ['REACT', 'HTML', 'CSS']},
+    { "name": "Anders Ravn", "subject": ['JS', 'HTML', 'NODEJS']},
+    { "name": "Daniel Fernandes", "subject": ['Database', 'HTML', 'CSS']}
+];
---
+++ javascript/javascript2/week6/Mentors/script.js
@@ -0,0 +1,61 @@
+console.log(mentors);
+
+let modifiedMentors = mentors.map(function (mentor) {
+    if (mentor["name"].length > 10) {
+        mentor.name = 'Ms ' + mentor.name;
+        return mentor;
+    } else {
+        mentor.name = 'Mr ' + mentor.name;
+        return mentor;
+    }
+});
+
+function filterMentorList(courseID) {
+    const resultHtml = document.getElementById('result');
+    let listHtml = '';
+
+
+
+    let filteresListByCourseId = modifiedMentors.filter(function (mentor) {
+        let sub = mentor.subject;
+        return sub.indexOf(courseID) >= 0;
+    });
+
+    filteresListByCourseId.forEach(function (mentor) {
+
+        listHtml += `<div>  ${mentor.name} </div>`;
+
+    });
+
+    resultHtml.innerHTML = listHtml;
+    console.log('courseID', resultHtml);
+
+}
+
+
+// mentors.forEach(mentor => listHtml += `<div>Mr ${mentor.name } </div>`);
+// mentors.forEach(function (mentor) {
+//     listHtml += `<div>Mr ${mentor.name } </div>`;
+// });
+//     listHtml += `<div>Mr ${mentor.name } </div>`;
+// };
+// mentors.forEach(findMentors);
+
+// let findMentors = (mentor) => {
+//     listHtml += `<div>Mr ${mentor.name } </div>`;
+// };
+// mentors.forEach(findMentors);
+
+// for (let i = 0; i < mentors.length; i++) {
+//     for (let j = 0; j < mentors["subject"].length; j++) {
+//         if (courseID === mentors[i].subject[j]) {
+//             listHtml += `<div>${mentors[i].name } </div>`;
+//         }
+//     }
+// }
+
+// let listHtml = '';
+// listHtml += '<div> Fowad</div>';
+// listHtml += '<div> Susane</div>';
+// listHtml += '<div> Sara</div>';
+// resultHtml.innerHTML = listHtml;
