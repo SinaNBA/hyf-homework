afea3bf90112710e19badd0376645391cd4b83ea
Sina Nejadbakhtiari <snejadbakhtiari@gmail.com>
2020-01-14 12:35:09 +0100

    Modified Homework

--- nodejs/week1/Meal sharing/src/backend/routes/meal.js
+++
@@ -1,6 +0,0 @@
-function meal(meals) {
-    let randomMeal = Math.floor(Math.random() * Math.floor(3));
-    return meals[randomMeal];
-};
-
-exports.meal = meal;
--- nodejs/week1/Meal sharing/src/backend/routes/reservation.js
+++
@@ -1,6 +0,0 @@
-function reservation(reservations) {
-    const randomReservation = Math.floor(Math.random() * Math.floor(2));
-    return reservations[randomReservation];
-};
-
-exports.reservation = reservation;
